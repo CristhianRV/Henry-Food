394662d396d499ea016a54030580f6a22479f08f
Cristhian Rios <cristian.riosv0330@gmail.com>
2023-01-24 15:12:42 -0500

    avance 24/01/2023

--- api/src/controllers/recipeControllers.js
+++ api/src/controllers/recipeControllers.js
@@ -1,90 +1,112 @@
 const { Recipe } = require("../db.js");
 const { Diet } = require("../db.js");
 const { cleanApi } = require("../utils/funciones.js");
-const  axios  = require("axios")
+const axios = require("axios");
 const { Op } = require("sequelize");
 require("dotenv").config;
 const { API_KEY2 } = process.env;
 
+const searchAll = async () => {
+  const dataApiRow = (
+    await axios.get(
+      "https://run.mocky.io/v3/84b3f19c-7642-4552-b69c-c53742badee5"
+      // `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY2}&addRecipeInformation=true&number=100`
+    )
+  ).data;
+  const dataApi = cleanApi(dataApiRow.results);
 
+  const dataBDD = await Recipe.findAll({
+    include: {
+      model: Diet,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
 
-const searchAll= async()=>{
-    const dataApiRow = (await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY2}&addRecipeInformation=true&number=100`)).data
-    const dataApi = cleanApi(dataApiRow.results);
+  return [...dataBDD, ...dataApi];
+};
 
-    const dataBDD = await Recipe.findAll({
-        
-        include: {
-          model: Diet,
-          attributes: ["name"],
-          through:{
-            attributes:[],
-        },
-        }
-    });
+const searchName = async (name) => {
+  const dataApiRow = (
+    await axios.get(
+      "https://run.mocky.io/v3/84b3f19c-7642-4552-b69c-c53742badee5"
+      // `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY2}&addRecipeInformation=true&number=100`
+    )
+  ).data;
+  const dataApi = cleanApi(dataApiRow.results);
+  const filterApi = dataApi.filter((element) =>
+    element.name.toLowerCase().includes(name.toLowerCase())
+  );
 
-    return [...dataBDD,...dataApi];
-}
-
-const searchName = async (name)=>{
-    const dataApiRow =(await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY2}&addRecipeInformation=true&number=100`)).data
-    const dataApi = cleanApi(dataApiRow.results);
-    const filterApi = dataApi.filter((element)=>element.name.toLowerCase().includes(name.toLowerCase()));
-
-    const dataBDD = await Recipe.findAll({where:{
-        name: {
-            [Op.iLike]: `%${name}%`,
-        },
+  const dataBDD = await Recipe.findAll({
+    where: {
+      name: {
+        [Op.iLike]: `%${name}%`,
+      },
     },
     include: {
-        model: Diet,
-        attributes: ["name"],
-        through:{
-            attributes:[],
-        },
-      }
-});
-    return [...dataBDD,...filterApi];
-    
-}
+      model: Diet,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
+  return [...dataBDD, ...filterApi];
+};
 
+const searchID = async (id, source) => {
+  const recipe =
+    source === "api"
+      ? (
+          await axios
+            .get
+            // `https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY2}`
+            ()
+        ).data
+      : await Recipe.findByPk(id);
 
+  if (source === "api") {
+    return {
+      id: recipe.id,
+      name: recipe.title,
+      description: recipe.summary,
+      healthScore: recipe.healthScore,
+      preparation: recipe.analyzedInstructions[0]?.steps
+        .map((step) => step.step)
+        .join(" "),
+      image: recipe.image,
+    };
+  }
 
+  return recipe;
+};
 
-const searchID =async (id, source)=>{
-    const recipe = source === "api"? (await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY2}`)).data:await Recipe.findByPk(id);
-
-    if(source === "api"){
-       return {
-            id: recipe.id,
-            name: recipe.title,
-            description: recipe.summary,
-            healthScore: recipe.healthScore,
-            preparation: recipe.analyzedInstructions[0]?.steps.map((step)=>step.step).join(" "),
-            image: recipe.image,
-        }
-    }
-
-
-    return recipe;
-
-}
-
-
-
-
-
-const createRecip = async (name, description, healthScore, preparation, diets, image) => {
-    const newRecip = await Recipe.create({name, description, healthScore, preparation, image});
-    const dietas = await Diet.findAll({where: {name:diets}});
-    await newRecip.addDiets(dietas);
-    return newRecip;
-}
+const createRecip = async (
+  name,
+  description,
+  healthScore,
+  preparation,
+  diets,
+  image
+) => {
+  const newRecip = await Recipe.create({
+    name,
+    description,
+    healthScore,
+    preparation,
+    image,
+  });
+  const dietas = await Diet.findAll({ where: { name: diets } });
+  await newRecip.addDiets(dietas);
+  return newRecip;
+};
 
 module.exports = {
-    createRecip,
-    searchID,
-    searchName,
-    searchAll,
-}
-
+  createRecip,
+  searchID,
+  searchName,
+  searchAll,
+};
--- api/src/utils/funciones.js
+++ api/src/utils/funciones.js
@@ -1,22 +1,20 @@
-
-
-
-const cleanApi = (api)=>{
-    const array= api.map((element)=>{
-        return {
-            id: element.id,
-            name: element.title,
-            description: element.summary,
-            healthScore: element.healthScore,
-            preparation: element.analyzedInstructions[0]?.steps.map((step)=>step.step).join(" "),
-            diets:element.diets.join(", ") ,
-            image: element.image,
-
-        }
-    });
-    return array
-}
+const cleanApi = (api) => {
+  const array = api.map((element) => {
+    return {
+      id: element.id,
+      name: element.title,
+      description: element.summary,
+      healthScore: element.healthScore,
+      preparation: element.analyzedInstructions[0]?.steps
+        .map((step) => step.step)
+        .join(" "),
+      diets: element.diets,
+      image: element.image,
+    };
+  });
+  return array;
+};
 
 module.exports = {
-    cleanApi,
-}
+  cleanApi,
+};
